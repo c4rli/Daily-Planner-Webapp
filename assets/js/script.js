a96ba5cf23e7b65da807e9185f1a2a03fcd561e7
xarli <117117311+c4rli@users.noreply.github.com>
2023-01-25 00:36:43 +0000

    add base timeblock generation js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,23 @@
 var currentDay = moment();
 $("#currentDay").text(currentDay.format("MMMM Do YYYY"));
+
+var contanerEl = $("#mainContainer");
+
+
+for (var i = 9; i < 17; i++){
+    createTimeblock(i);
+}
+
+function createTimeblock(time) {
+    var timeblock = $("<div>").addClass("row");
+
+    var blockTime = $("<div>").text(time + ":00").addClass("hour px-3");
+    var blockText = $("<textarea>").addClass("time-block past flex-grow-1");
+    var blockSave = $("<button>").text("Save").addClass("saveBtn");
+
+    timeblock.append(blockTime);
+    timeblock.append(blockText);
+    timeblock.append(blockSave);
+    contanerEl.append(timeblock);
+}
+
