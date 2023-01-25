509d7b2e6c73318875c40508f716b375d2f7936b
xarli <117117311+c4rli@users.noreply.github.com>
2023-01-25 23:47:55 +0000

    Add time generating array function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,58 @@
-var currentDay = moment();
-$("#currentDay").text(currentDay.format("MMMM Do YYYY"));
+var eventsArray = [];
+var startTime = 9;
+var endTime = 17;
+
+function generateArray() {
+    if (eventsArray.length == 0) {
+        eventsArray = [];
+        for (let x = startTime; x <= endTime; x++) {
+            var time = moment(x, "h").format("h a");
+            eventsArray.push([time, ""]);
+        }
+    }
+    // $("#4a").text(time.format("h:mm:s a"));
+}
+
+function generateTimeblocks() {
+    eventsArray.forEach(function(x){
+        createTimeblock(x[0]);
+    })
+}
+
+var currentDayTime = moment("25 March 2023 13:30", "D MMMM YYYY h:mm");
+// var currentDayTime = moment();
+$("#currentDay").text(currentDayTime.format("MMMM Do YYYY h:mm a"));
 
 var contanerEl = $("#mainContainer");
 
 
-for (var i = 9; i < 17; i++){
-    createTimeblock(i);
-}
+
+
+generateArray();
+generateTimeblocks();
 
 function createTimeblock(time) {
     var timeblock = $("<div>").addClass("row");
 
-    var blockTime = $("<div>").text(time + ":00").addClass("hour px-3");
-    var blockText = $("<textarea>").addClass("time-block past flex-grow-1");
+    var blockTime = $("<div>").text(time).addClass("hour px-3");
+    var blockText = $("<textarea>").addClass("time-block flex-grow-1");
     var blockSave = $("<button>").text("Save").addClass("saveBtn");
+
+    // moment(time,"h a").format()
+    
+    // if (moment(time,"h a").isBefore(currentDayTime.format("h a"))) {
+    //     blockText.addClass("past");
+    // } 
+
+    if (moment(time,"h a").isBefore(currentDayTime.format("h a"))) {
+        blockText.addClass("past");
+    } 
+    // else if (moment(time,"h a").isAfter(currentDayTime.format("h a"))) {
+    //     blockText.addClass("future");
+    // } 
+    // if (time == currentDayTime.format("h a")){
+    //     blockText.addClass("present");
+    // }
 
     timeblock.append(blockTime);
     timeblock.append(blockText);
