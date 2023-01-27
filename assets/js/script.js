9ce6d581cd74e03ee0d38d1982f23cfb72fcb46e
xarli <117117311+c4rli@users.noreply.github.com>
2023-01-27 23:37:01 +0000

    Add function to capture data in <textarea>

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,26 @@ var endTime = 20;
 function generateArray() {
     if (eventsArray.length == 0) {
         eventsArray = [];
-        for (let x = startTime; x <= endTime; x++) {
-            var time = moment(x, "h")
-            // .format("h a");
-            eventsArray.push([time, ""]);
+        for (var i = startTime; i <= endTime; i++) {
+            var time = moment(i, "h")
+            eventsArray.push([time, i]);
         }
     }
 }
 
 function generateTimeblocks() {
-    eventsArray.forEach(function (x) {
-        createTimeblock(x[0]);
+    eventsArray.forEach(function (x, index) {
+        createTimeblock(x, index);
     })
 }
 
-function createTimeblock(time) {
-    var timeblock = $("<div>").addClass("row");
+function createTimeblock(blockArray, index) {
+    var time = blockArray[0];
+    var eventTexr = blockArray[1];
 
-    var blockTime = $("<div>").text(time.format("h a")).addClass("hour px-3");
-    var blockText = $("<textarea>").addClass("time-block flex-grow-1");
+    var timeblock = $("<div>").addClass("row").attr("data-index", index);
+    var blockTime = $("<div>").text(time.format("h A")).addClass("hour px-3");
+    var blockText = $("<textarea>").text(eventTexr).addClass("time-block flex-grow-1");
     var blockSave = $("<button>").text("Save").addClass("saveBtn");
 
     if (moment(time).isSame(currentDayTime, "hour")) {
@@ -44,6 +45,14 @@ var currentDayTime = moment();
 $("#currentDay").text(currentDayTime.format("MMMM Do YYYY h:mm a"));
 
 var contanerEl = $("#mainContainer");
+
+contanerEl.on("click", ".saveBtn", function (event) {
+    var selectedEvent = $(event.target)
+    console.log(selectedEvent.text());
+    console.log(selectedEvent.parent().attr("data-index"));
+    eventsArray[selectedEvent.parent().attr("data-index")][1] = selectedEvent.parent().children("textarea").val();
+
+});
 
 generateArray();
 generateTimeblocks();
